eb9be6e450cf7320b26b2434ecb1af2df4359ff8
freddymuleya16 <80630982+freddymuleya16@users.noreply.github.com>
2021-10-23 14:24:31 +0200

    Cleaning Controlers code

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -1,86 +1,52 @@
 <?php
-
 class homeController extends controller
 {
-
-	
 	public function index($id='',$name=''){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
-
-		//echo "ID: is ". $id.' and name is : '. $name;
-		//var_dump($this->model);
-
 		
 		$this->model('music',[]);
 		$musicModel= new Music();
-		 //var_dump($musicModel->getComments());
-
 		
 		$this->view('home\index',[
-
 			'name'=> $name,
 			'id'=> $id,
 			[$musicModel->getAllsong()],
 			[$musicModel->getComments()]
 
 		]);
-		
-		//$this->model->render1();
 		$this->view->render();
-		//$this->showLikes('1');
-		//$b=new Music;$b->getLikers(1);
-
 	}
-	
-	
-
 	public function say(){
 		new Cheeese;
 	}
 	public function aboutUS(){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
-
 		$this->view('home\aboutUs');
 		$this->view->render();
 	}
 
 	public function register(){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
 		redirect('users\register');
 		$this->view('home\register',$data);
 		$this->view->render();
 	}
 
 	public function login(){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
 		redirect('users\login');
 		$this->view('home\login',$data);
 		$this->view->render();
 	}
 	public function contact(){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
 		redirect('users\contact');
 		$this->view('home\contact',$data);
 		$this->view->render();
 	}
 
 	public function admin(){
-		//echo "i am in in". __CLASS__ .'method'.__METHOD__;
-		//$this->model('Music',[]);
-		//$this->model->render1();
 		redirect('users\admin');
-		//$this->view('home\admin',$data);
-
-		
-		//$this->view->render();
-	}
+		}
 
 	public function load_comments($commentCount){
-		//echo $commentCount;
 		$this->model('music',[]);
-		
 		$model = new Music;
-
 		$comments=$model->showMoreComments($commentCount);
 		foreach ($comments as $row) {
 			echo "<p>";
@@ -90,18 +56,13 @@ class homeController extends controller
 	}
 
 	public function submitComment(){
-
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
 	        $info = [
-	            
 	            "author" => trim($_POST["author"]),
 	            "comment" => trim($_POST["comment"])
 	        ];
 	        $this->model('music',[]);
-		
 		$model = new Music;
-			
-
 			$ex=$model->addComment($info);
 			if($ex){
 				echo $info['author'].": ".$info['comment'];
@@ -109,7 +70,6 @@ class homeController extends controller
 				echo "Ooops something went wrong";
 			}
 		}
-		
 	}
 
 	public function addLike($songId){
@@ -121,17 +81,11 @@ class homeController extends controller
 
 			if($model->liked($songId)){
 				echo "<p>".$model->addLike($songId)."</p>";
-				
-				//echo "<script>alert('adding')</script>";
 			}else{
-				
 				echo "<p>".$model->removeLike($songId)."</p>";
-				//echo "<script>alert('removed')</script>";
 			}
 		}
-		
 	}
-
 	public function addDislike($songId){
 		if (!isLoggedIn()) {
 			echo "<a href=".URLROOT."users/login>LOGIN</a>";
@@ -146,27 +100,19 @@ class homeController extends controller
 				$model->removeDislike($songId);
 			}
 		}
-		
 	}
 
 	public function loadLike($id){
 		$this->model('music',[]);
 		$model=new Music;
-
 		$row = $model->getReaction($id);
-
     	echo "<p>".$row['Likes']."</p>";
-    	//echo "<span class='tooltiptext'>";$model->getReactors($id,"LIKE");echo "</span>";
 	}
 
 	public function loadDislike($id){
 		$this->model('music',[]);
 		$model=new Music;
-
 		$row = $model->getReaction($id);
-
     	echo "<p>".$row['Dislikes']."</p>";
-    	
-    	//echo "<span class='tooltiptext'>";$model->getReactors($id,"DISLIKE");echo "</span>";
 	}
 }
